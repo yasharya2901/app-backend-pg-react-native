41bcbc68827a151327a684fb9ac1d5ff927705a4
yasharya2901 <yasharya2901@gmail.com>
2024-07-14 00:12:08 +0530

    Fix date parsing with await

--- routes/menu.js
+++ routes/menu.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllMenu, createMenu} = require('../controllers/menu');
+const {getAllMenu, createMenu, getCurrentWeekMenu} = require('../controllers/menu');
 
 router.route('/').get(getAllMenu).post(createMenu);
+router.route("/week").get(getCurrentWeekMenu);
 
 module.exports = router;
